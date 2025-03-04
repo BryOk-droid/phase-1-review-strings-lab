daa3d46361d68daae2683bff9315475f23e1d26b
BryOk-droid <brianokoth534@gmail.com>
2025-03-04 14:48:56 +0300

    Completed strings lab with all tests passing

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+// Define the current user
+const currentUser = "Grace Hopper";
+
+// Construct the welcome message using template literals
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Create the excited version of the welcome message
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Create a short greeting using only the first letter of currentUser
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
